b9818f4b16b5441b2face211bff2ffeb21022865
Tyler Cole <involution124@gmail.com>
2020-02-13 09:15:56 -0800

    Working consumer

--- DockerPythonConsumer/app.py
+++ DockerPythonConsumer/app.py
@@ -1,33 +1,21 @@
-from kafka import KafkaProducer
+from kafka import KafkaConsumer
 import os
 import sys
-import tarfile
-import urllib.request
 
-dataURL = "https://zenodo.org/record/815657/files/P2E_S5.tar.xz"
-dataFileName = "file.tar.xz"
+print("Here");
 kafka_host = os.getenv('KAFKA_HOST_NAME')
-os.system("env");
 if(kafka_host == None):
     print("Failed, no KAFKA_HOST_NAME environment variable was set")
     sys.exit(1)
 
-urllib.request.urlretrieve(dataURL, dataFileName)
-tar = tarfile.open(dataFileName, "r:xz")
-tar.extractall()
-tar = tarfile.open("P2E_S5_C1.tar.xz", "r:xz")
-tar.extractall()
-print("Extracted data")
-producer = KafkaProducer(bootstrap_servers=kafka_host)
-for filename in os.listdir("P2E_S5_C1.1"):
-    with open("P2E_S5_C1.1/" + filename, "rb") as image:
-        f = image.read()
-        b = bytearray(f) 
-        print("Message sent")
-        response = producer.send('images', b)
-        print("Resposne = " + str(response))
-        print("Fetching the message");
-        result = response.get(timeout=30)
-        print("Result = " + str(result))
-
-
+print("Got here!")
+numIterations = 0;
+consumer  = KafkaConsumer("images", group_id="processor",  bootstrap_servers=kafka_host)
+print("Consumder set up")
+iterator = iter(consumer);
+print(iterator);
+print(next(iterator, b"A"));
+#for message in consumer:
+#    pass ; 
+#    print("Message : ) ")
+#    print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,message.offset, message.key, message.value))
