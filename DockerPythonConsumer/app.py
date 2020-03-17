cd406f3d4f7390d988811931ca2edd973f2aa06b
Arnav Bhutani <bhutania@oregonstate.edu>
2020-03-16 17:43:16 -0700

    more bugfixes

--- DockerPythonConsumer/app.py
+++ DockerPythonConsumer/app.py
@@ -33,12 +33,12 @@ if(kafka_host == None):
     print("Failed, no KAFKA_HOST_NAME environment variable was set")
     sys.exit(1)
 
-def get_image(parsed_kafka_msg):
-    cache = tempfile.NamedTemporaryFile(delete=False, suffix=".jpg", prefix="classify")
-#    req = urllib.request.urlopen(parsed_kafka_msg["url"])
-    cache.write(parsed_kafaka_msg[])
-    cache.close()
-    return cache
+# def get_image(parsed_kafka_msg):
+#     cache = tempfile.NamedTemporaryFile(delete=False, suffix=".jpg", prefix="classify")
+# #    req = urllib.request.urlopen(parsed_kafka_msg["url"])
+#     cache.write(parsed_kafaka_msg)
+#     cache.close()
+#     return cache
 
 if __name__ == "__main__":
     print("starting main")
@@ -66,11 +66,15 @@ if __name__ == "__main__":
         #print("Message : ) ")
         #print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,message.offset, message.key, message.value))
         print("Got to pre-cache")
-        filecache = get_image(message.value)
-        process = subprocess.Popen("Darknet detector test ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3_weights -i 0 -thresh 0.25 "+cmd.format(file=os.path.abspath(filecache.name)).split())
+        # filecache = get_image(message.value)
+        cache = tempfile.NamedTemporaryFile(delete=False, suffix=".jpg", prefix="classify")
+#       req = urllib.request.urlopen(parsed_kafka_msg["url"])
+        cache.write(message)
+        cache.close()
+        process = subprocess.Popen("Darknet detector test ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3_weights -i 0 -thresh 0.25 "+cmd.format(file=os.path.abspath(cache.name)).split())
         print("Got to post-cache")
         process.wait()
-        filecache.close()
+        # filecache.close()
         print("finished cache")
 
 # def get_image(parsed_kafka_msg):
