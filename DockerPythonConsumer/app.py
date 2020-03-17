bdcc9f32495dd0316681277d1a12d782506782e6
Arnav Bhutani <bhutania@oregonstate.edu>
2020-03-17 00:26:24 -0700

    fixed split

--- DockerPythonConsumer/app.py
+++ DockerPythonConsumer/app.py
@@ -11,7 +11,7 @@ if(kafka_host == None):
     sys.exit(1)
 
 if __name__ == "__main__":
-    cmd = '/usr/local/bin/darknet detector test ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3.weights -i 0 -thresh 0.25 {file}'.split()
+    cmd = '/usr/local/bin/darknet detector test ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3.weights -i 0 -thresh 0.25 {file}'
 
     consumer  = KafkaConsumer("images", group_id="processor",  request_timeout_ms=120000,
                                 session_timeout_ms=100000, bootstrap_servers=kafka_host)
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         cache.write(bytes(message.value))
         cache.close()
         filecache = cache
-        process = subprocess.Popen(cmd.format(file=os.path.abspath(filecache.name)), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        process = subprocess.Popen(cmd.format(file=os.path.abspath(filecache.name)).split(), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
         out, err = process.communicate()
         print("stdout: %s", out)
         print("stderr: %s", err)
