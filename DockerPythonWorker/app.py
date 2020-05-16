d9499b0693d76bbd4b16b48a5c697be1bb4877bb
Tyler Cole <involution124@gmail.com>
2020-05-15 23:25:40 -0700

    Finaly working commit

--- DockerPythonWorker/app.py
+++ DockerPythonWorker/app.py
@@ -10,12 +10,14 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import io
+import tarfile
+import json
 print("We are here");
 kafka_host = os.getenv('KAFKA_HOST_NAME')
 if(kafka_host == None):
     kafka_host = "kafka-service-train";
 
-consumer  = KafkaConsumer("models-to-fit", group_id="worker",  request_timeout_ms=120000,session_timeout_ms=100000, bootstrap_servers=kafka_host)
+consumer  = KafkaConsumer("models-to-fit", group_id="worker",  fetch_max_bytes=2000000000, request_timeout_ms=120000,session_timeout_ms=100000, bootstrap_servers=kafka_host)
 
 while(1):
     print("Starting the worker process");
@@ -25,15 +27,15 @@ while(1):
         file_like_object = io.BytesIO(bytestream)
         tar = tarfile.open(fileobj=file_like_object)
         for member in tar.getmembers():
-            f = tar.extractfile(member);
-            print("F = " + str(f));
+            print("F = " + str(member.name));
+        tar.extractall()
         break;
 
     print("Model receieved");
     model = keras.models.load_model("/model.h5")
 
-    input_x = np.fromfile("input_x"); 
-    input_y = np.fromfile("input_y");
+    input_x = np.fromfile("input_x", dtype = np.uint32); 
+    input_y = np.fromfile("input_y", dtype = np.uint32);
     print("X and Y are loaded", str(input_x.shape), str(input_y.shape));
 
     metadata_file = open("metadata");
@@ -43,8 +45,12 @@ while(1):
     index =  metadata["index"];
     training_epochs = metadata["training_epochs"];
     validation_split = metadata["validation_split"];
-    
-    score = model.fit(input_x, input_y, epochs=training_epochs, validation_split=validation_split, batch_size=128)
+    x_shape = metadata["x_shape"];
+    y_shape = metadata["y_shape"];
+    input_x = np.reshape(input_x, x_shape);
+    input_y = np.reshape(input_y, y_shape);
+    print("New shape = " + str(input_x.shape));
+    score = model.fit(input_x, input_y, epochs=training_epochs, validation_split=float(validation_split), batch_size=128)
     producer = KafkaProducer(bootstrap_servers=kafka_host)
     print("index, score = ", + str(index) + " "  + str(score));
     producer.send('models-fitted', {index: score})
